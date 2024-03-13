eed794b6b5f25e41f609f66ce52d85e2b5df59fa
Baruk Freitas <baruk.freitas@gmail.com>
2024-03-13 20:04:55 -0300

    Bug fix
    Função de pesquisa de restaurantes consertada e agora mostra todas as informações do restaurante juntamente ao botão de fazer reserva.

--- script.js
+++ script.js
@@ -3,24 +3,70 @@
 //     window.location.href = "fazerPedido.html"
 // };
 
-function search() {
+// function search() {
+//     let input = document.getElementById('searchBar').value;
+//     input = input.toLowerCase();
+//     let pesquisaTitulos = document.getElementsByClassName('r_titulo');
+//     let pesquisaEnderecos = document.getElementsByClassName('r_endereco');
+
+//     for (let i = 0; i < pesquisaTitulos.length; i++) {
+//         if (!pesquisaTitulos[i].innerHTML.toLowerCase().includes(input)) {
+//             pesquisaTitulos[i].style.display = "none";
+//             pesquisaEnderecos[i].style.display = "none";
+//         } else {
+//             pesquisaTitulos[i].style.display = "list-item";
+//             pesquisaEnderecos[i].style.display = "block";
+//         }
+//     }
+// }
+
+
+// function search(){
+//     let input = document.getElementById('searchBar').value;
+//     input = input.toLowerCase();
+//     let titulo = document.getElementsByClassName('r_titulo');
+//     let rua = document.getElementsByClassName('rua');
+//     let numero = document.getElementsByClassName('n-fone');
+
+//     for(i = 0; i < numero.length; i++){
+//         if(!numero[i].innerHTML.toLowerCase().includes(input)){
+//             numero[i].style.display='none';
+//         }else{
+//             numero[i].style.display='list-item';
+//         }
+//     }    
+
+//     for(i = 0; i < rua.length; i++){
+//         if(!rua[i].innerHTML.toLowerCase().includes(input)){
+//             rua[i].style.display='none';
+//         }else{
+//             rua[i].style.display='list-item';
+//         }
+//     }
+
+//     for(i = 0; i < titulo.length; i++){
+//         if(!titulo[i].innerHTML.toLowerCase().includes(input)){
+//             titulo[i].style.display='none';
+//         }else{
+//             titulo[i].style.display='list-item';
+//         }
+//     }
+// }
+
+function search(){
     let input = document.getElementById('searchBar').value;
     input = input.toLowerCase();
-    let pesquisaTitulos = document.getElementsByClassName('r_titulo');
-    let pesquisaEnderecos = document.getElementsByClassName('r_endereco');
-
-    for (let i = 0; i < pesquisaTitulos.length; i++) {
-        if (!pesquisaTitulos[i].innerHTML.toLowerCase().includes(input)) {
-            pesquisaTitulos[i].style.display = "none";
-            pesquisaEnderecos[i].style.display = "none";
-        } else {
-            pesquisaTitulos[i].style.display = "list-item";
-            pesquisaEnderecos[i].style.display = "block";
+    let info = document.getElementsByClassName('info');
+    
+    for(i = 0; i < info.length; i++){
+        if(!info[i].innerHTML.toLowerCase().includes(input)){
+            info[i].style.display='none';
+        }else{
+            info[i].style.display='list-item';
+            info[i].style.display='flex';
         }
-    }
+    }    
 }
-
-
 // Função para carregar um arquivo JSON via requisição HTTP (Ajax)
 function carregarJSON(caminho, callback) {
     var xhr = new XMLHttpRequest();
@@ -60,3 +106,4 @@ function exibirCampoCEP() {
 
   // Adiciona evento de clique ao link "Garanhuns - PE"
   document.getElementById('localizacao').addEventListener('click', exibirCampoCEP);
+
