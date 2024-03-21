8d7adcb4e4e4c2f79b97c1f65f8df28c18edf02b
Baruk Freitas <baruk.freitas@gmail.com>
2024-03-21 20:33:27 -0300

    ReactJS
    Começando a converter o projeto para ReactJS com NodeJS

---
+++ leBruine/src/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import logo from './assets/le-Bruine-logo.png'
+import lupa from './assets/lupa.svg';
+import loginIcon from './assets/login-ico.svg'
+import '../src/header.css'
+
+ReactDOM.createRoot(document.getElementById('header')).render(
+    <div className='raiz'>
+        <a href="#"><div className='logo'><img src={logo} alt="logo" /></div></a>
+        <div className='search_area'><input type="text" placeholder='Busque por restaurante, comida, cidade...' className='search_bar' id='searchBar'/><button className='button_search'><img src={lupa} alt="lupa" /></button></div>
+        <div className='options'>
+            <a href="#"><div className='menu'>Fazer Reserva</div></a>
+            <a href="#"><div className='menu'>Minhas Reservas</div></a>
+            <a href="#"><div className='menu'>Restaurantes</div></a>
+        </div>
+        <a href="login_html"><div className='login'>
+            <img src={loginIcon} alt="icone de login" />
+            <p className='font_login'>Login</p>
+            </div></a>
+    </div>
+)
