43e2918873b2314bf35af663a46248013f6f71d4
Thiagoanizio <thiagomi1912@gmail.com>
2024-04-08 21:03:00 -0300

    correção no footer e página de início finalizada

--- leBruine/src/myPlace.jsx
+++ leBruine/src/myPlace.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('myPlace')).render(
         <div className="myPlace">
             <img src={pin} alt="pin mapa" className="pin_map" />
             <p className="text">Você está em: <a href="#" className="city">Garanhuns-PE</a></p>
-            <p className="text_minor">Não é o seu local? Clique na cidade e escolha seu endereço    </p>
+            <p className="text_minor">Não é o seu local? Clique na cidade e escolha seu endereço</p>
         </div>
     </div>
 )
