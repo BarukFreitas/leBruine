3800ebe360692ec77c0e9f7992e7a70e2d1167fd
Baruk Freitas <baruk.freitas@gmail.com>
2024-04-23 19:49:30 -0300

    Home arrumada

--- leBruine/src/App.jsx
+++ leBruine/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Restaurantes from './restaurantes'
 
 const App = () => {
   return (
     <div>
-      <p>NavBar</p>
-      <h1>React Router</h1>
       <Outlet />
-      <p>Footer</p>
+      <Restaurantes />
     </div>
   )
 }
--- leBruine/src/routes/Home.jsx
+++ leBruine/src/routes/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../header'
+import MyPlace from '../myPlace'
+import Carousel from '../carousel'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Header />
+      <MyPlace />
+      <Carousel />
     </div>
   )
 }
